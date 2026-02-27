f442cd11172e26c26e5b7a723b26214a6ad0db9f
sainath <sainathdussa1002@gmail.com>
2026-02-27 12:16:14 +0530

    max.c

---
+++ max.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int max, num1, num2;
+    printf("Enter the two numbers: \n");
+    scanf("%d %d",&num1, &num2);
+
+    if(num1 > num2)
+    
+ 	printf("Number 1 is greater:%d \n",num1);
+    else
+	    printf("Number 2 is greater:%d \n", num2);
+
+		    return 0;
+}
+
